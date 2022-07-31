75b40dd6d61c538b3833a2856f3929f5d7c8e8a7
mektigboy <mektigboy@protonmail.com>
2022-07-31 14:19:32 -0500

    Update deploy/01-deploy-raffle.js

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -3,6 +3,8 @@ const {
     developmentChains,
     networkConfig,
 } = require("../helper-hardhat-config");
+const { verify } = require("../helper-hardhat-config");
+
 const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2");
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
@@ -47,4 +49,15 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         args: args,
         waitConfirmations: network.config.blockConfirmations || 1,
     });
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        log("Verifying...");
+        await verify(raffle.address, args);
+        log("--------------------------------------------------");
+    }
 };
+
+module.exports.tags = ["all", "raffle"]
