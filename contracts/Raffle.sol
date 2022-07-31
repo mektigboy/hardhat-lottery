f1063c92dbc60119e67399765df450497d699c21
mektigboy <mektigboy@protonmail.com>
2022-07-30 21:51:58 -0500

    Update Raffle.sol

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,11 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughEtherEntered();
 
+/// @title Raffle Contract
+/// @author mektigboy
+/// @notice Creates a sample raffle contract.
+/// @dev Uses VRF.
 contract Raffle {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+
+    event RaffleEnter(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
@@ -14,7 +22,12 @@ contract Raffle {
     function enterRaffle() public payable {
         if (msg.value < i_entranceFee) revert Raffle__NotEnoughEtherEntered();
         s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
     }
+
+    function pickRandomWinner() external {}
+
+    function fullfillRandomWords() internal override {}
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
