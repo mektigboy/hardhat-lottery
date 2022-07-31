e7e84896370441836c117aa157a971105acf967a
mektigboy <mektigboy@protonmail.com>
2022-07-30 19:49:39 -0500

    Update Raffle.sol

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-contract Raffle {}
+error Raffle__NotEnoughEtherEntered();
+
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) revert Raffle__NotEnoughEtherEntered();
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+}
