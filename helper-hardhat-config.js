40ccf06ac5a0e202d92df6ec748fd4a7cee55ab2
mektigboy <mektigboy@protonmail.com>
2022-08-01 09:25:01 -0500

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -7,7 +7,7 @@ const networkConfig = {
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane:
             "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-        subscriptionid: "0",
+        subscriptionId: "0",
         callbackGasLimit: "500000", // 500,000
         interval: "30", // 30 seconds
     },
